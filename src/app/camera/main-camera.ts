d043cb2b23c1e03e4df44c8f3f400dc79aedaec2
Mamix84 <38494340+Mamix84@users.noreply.github.com>
2019-02-27 07:03:12 +0100

    Update main-camera.ts

--- src/app/camera/main-camera.ts
+++ src/app/camera/main-camera.ts
@@ -5,14 +5,14 @@ export class MainCamera extends Camera {
     constructor() {
         super();
 
-        this.createCamera();
+        super.camera = this.createCamera();
     }
 
     createCamera(): Camera {
         let camera = new PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
-        this.position.x = 0;
-        this.position.y = 5;
-        this.position.z = 5;
+        camera.position.x = 0;
+        camera.position.y = 5;
+        camera.position.z = 5;
         return camera;
     }
 }
